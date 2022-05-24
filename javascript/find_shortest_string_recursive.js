abbf699f922c25dc2dec932c84811f72efeabca6
Granit Zymeri <gzymeri55@gmail.com>
2022-05-24 19:07:33 -0400

    done

--- javascript/find_shortest_string_recursive.js
+++ javascript/find_shortest_string_recursive.js
@@ -1,5 +1,14 @@
 function findShortestStringRecursive(arr) {
-  // type your code here
+  // use if statement to check if the length of the array is === to 1
+  if (arr.length === 1) {
+    // if yes, the first element
+    return arr[0];
+  }
+  //declare a variable and assign the value of the function with the input of arr.slice(1) to remove the elemented already checked
+  const result = findShortestStringRecursive(arr.slice(1));
+
+  //check to see if the element is smaller or equal to the result length. If yes, return the element, else return result.
+  return arr[0].length <= result.length ? arr[0] : result;
 }
 
 if (require.main === module) {
